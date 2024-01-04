589b77d2add80914b9164dae2f2e0b71ae15741b
jagwirej <131268050+jagwirej@users.noreply.github.com>
2024-01-04 10:16:14 -0600

    High/Low Delta
    Gets a delta between the CPU usage of the Highest node and the Lowest node. If the difference is greater than or equal to 5%, then migration will commence.
    
    (Ex. node1 is at 10% and node2 is at 5%, migration will complete)
    (Ex. node1 is at 10% and node2 is at 12%, migration will skip)

--- loadbalance/getleastusednode.sh
+++ loadbalance/getleastusednode.sh
@@ -8,47 +8,64 @@ i=0
 
 for node in "${nodes[@]}"; do
 
-	#Assign name to variable for ease of use
-	name=$node
+        #Assign name to variable for ease of use
+        name=$node
 
-	#Get CPU usage of node
-	usage=$(ssh root@$name mpstat 1 1 | awk 'NR>3 && $12 ~ /[0-9.]+/ { print 100 - $13; exit }')
+        #Get CPU usage of node
+        usage=$(ssh root@$name mpstat 1 1 | awk 'NR>3 && $12 ~ /[0-9.]+/ { print 100 - $13; exit }')
 
-	#Convert float point to integer for comparison
-	usage=$(printf "%.0f" $usage)
+        #Convert float point to integer for comparison
+        usage=$(printf "%.0f" $usage)
 
-	if [ $i -eq 0 ]; then
+        if [ $i -eq 0 ]; then
 
-		#Sets first server as lowest use
-		minServer=$name
-		minUsage=$usage
-	fi
+                #Sets first server as min use
+                minServer=$name
+                minUsage=$usage
+		maxUsage=$usage
+        fi
 
-	if [ $i -gt 0 ]; then
+        if [ $i -gt 0 ]; then
 
-		#Checks to see if the current usage is different than the least usage
-		if [ $usage != $minUsage ]; then
-		        balance=true
+                #Checks to see if the current usage is different than the least usage
+                if [ $usage != $minUsage ]; then
+                        balance=true
+                fi
+
+                #Checks to see if current usage is lower than the least usage
+                if [ $usage -lt $minUsage ]; then
+                        minServer=$name
+                        minUsage=$usage
+                fi
+
+		#Checks to see if current usage is also the max usage
+		if [ $usage -gt $maxUsage ]; then
+			maxUsage=$usage
 		fi
 
-		#Checks to see if current usage is lower than the least usage
-		if [ $usage -lt $minUsage ]; then
-        		minServer=$name
-        		minUsage=$usage
-		fi
+        fi
 
-	fi
-
-	((i++))
+        ((i++))
 done
 
-
-#At least some server usages were unique, so return result
-if [ $balance == "true" ]; then
-	echo "${minServer}"
-fi
+#Find the delta between the max usage and min usage to determine if a move is necessary
+usageDelta=$(($maxUsage - $minUsage))
 
 #All server usages were equal, so return null
 if [ $balance == "false" ]; then
+        echo "NULL"
+fi
+
+
+#If delta is 5 or greater, then pass on the lowest used node
+if [ $usageDelta -ge 5 ]; then
+
+	#At least some server usages were unique, so return result
+	if [ $balance == "true" ]; then
+        	echo "${minServer}"
+	fi
+else
+
+	#If delta is below 5, pass NULL
 	echo "NULL"
 fi
--- loadbalance/getmostusednode.sh
+++ loadbalance/getmostusednode.sh
@@ -22,34 +22,50 @@ for node in "${nodes[@]}"; do
                 #Sets first server as highest use
                 maxServer=$name
                 maxUsage=$usage
+		minUsage=$usage
         fi
 
         if [ $i -gt 0 ]; then
 
-                #Checks to see if the current usage is different than the least usage
+                #Checks to see if the current usage is different than the max usage
                 if [ $usage != $maxUsage ]; then
                         balance=true
                 fi
 
-                #Checks to see if current usage is lower than the least usage
+                #Checks to see if current usage is greater than the max usage
                 if [ $usage -gt $maxUsage ]; then
                         maxServer=$name
                         maxUsage=$usage
                 fi
+
+		#Checks to see if current usage is also the min usage
+		if [ $usage -lt $minUsage ]; then
+			minUsage=$usage
+		fi
 
         fi
 
         ((i++))
 done
 
-
-#At least some server usages were unique, so return result
-if [ $balance == "true" ]; then
-        echo "${maxServer}"
-fi
-
+#Find the delta between the max usage and min usage to determine if a move is necessary
+usageDelta=$(($maxUsage - $minUsage))
 
 #All server usages were equal, so return null
 if [ $balance == "false" ]; then
         echo "NULL"
+fi
+
+
+#If delta is 5 or greater, then pass on the highest used node
+if [ $usageDelta -ge 5 ]; then
+
+	#At least some server usages were unique, so return result
+	if [ $balance == "true" ]; then
+        	echo "${maxServer}"
+	fi
+else
+
+	#If delta is below 5, pass NULL
+	echo "NULL"
 fi
