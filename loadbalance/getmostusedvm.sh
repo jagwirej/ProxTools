a87ce85ae59e6bc7a4057089d04ff785d7ebdee7
jagwirej <131268050+jagwirej@users.noreply.github.com>
2024-05-20 12:45:53 -0500

    Backup/Locked VMs
    Added exception handling for VMs that are locked for a current migration or backup

--- loadbalance/getmostusedvm.sh
+++ loadbalance/getmostusedvm.sh
@@ -10,6 +10,9 @@ lastvm=$(($lastvm + 0))
 #Get all running vms on node given as a parameter
 vmstring=$(bash /proxtools/list/getvmsonnode.sh $node)
 
+#Get skip backup vm
+skipbackupvm=$(cat "/proxtools/loadbalance/skipbackupvm.txt" 2>/dev/null)
+
 vms=($vmstring)
 
 #Setup i as variable to iterate to determine which vm is the first in the list
@@ -18,39 +21,43 @@ i=0
 #For loop to iterate through all running vms
 for vm in "${vms[@]}"; do
 
-	#Check if vm is last vm moved
-	if [ $vm -eq $lastvm ]; then
-		continue
-	fi
+        #Check if vm is last vm moved
+        if [ $vm -eq $lastvm ]; then
+                continue
+        fi
+        #Check if vm is currently locked for backup
+        if [[ "$vm" == "$skipbackupvm" ]]; then
+                continue
+        fi
 
-	#Get CPU usage of specific VM
-	usage=$(bash /proxtools/loadbalance/getvmload.sh $node $vm)
+        #Get CPU usage of specific VM
+        usage=$(bash /proxtools/loadbalance/getvmload.sh $node $vm)
 
-	#If this is the first iteration, assign usage to maxusage
-	if [ "$i" -lt 1 ]; then
-		maxvm=$vm
-		maxusage=$usage
-	fi
+        #If this is the first iteration, assign usage to maxusage
+        if [ "$i" -lt 1 ]; then
+                maxvm=$vm
+                maxusage=$usage
+        fi
 
-	#If current VM usage is higher than recorded maxusage, set maxusage to current usage
-	if (( $(echo "$usage > $maxusage" | bc -l) )); then
-		maxvm=$vm
-		maxusage=$usage
-	fi
+        #If current VM usage is higher than recorded maxusage, set maxusage to current usage
+        if (( $(echo "$usage > $maxusage" | bc -l) )); then
+                maxvm=$vm
+                maxusage=$usage
+        fi
 
-	#Itterate i
-	((i++))
+        #Itterate i
+        ((i++))
 done
 
 if [ "$maxvm" = "" ]; then
-	#Return NULL if no output for error handling
+        #Return NULL if no output for error handling
         echo "NULL"
 
-	#Write last migrated vm to file
-	echo "NULL" > "/proxtools/loadbalance/lastmigratedvm.txt"
+        #Write last migrated vm to file
+        echo "NULL" > "/proxtools/loadbalance/lastmigratedvm.txt"
 else
         #Write last migrated vm to file
-	echo "$maxvm" > "/proxtools/loadbalance/lastmigratedvm.txt"
-	#Return highest used vmid
-	echo $maxvm
+        echo "$maxvm" > "/proxtools/loadbalance/lastmigratedvm.txt"
+        #Return highest used vmid
+        echo $maxvm
 fi
