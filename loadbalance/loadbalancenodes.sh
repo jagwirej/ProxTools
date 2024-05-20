8cd1043f1ef33ee37792f41a33913e2a9a230c11
jagwirej <131268050+jagwirej@users.noreply.github.com>
2024-05-20 12:49:15 -0500

    Backup/Locked VMs - Email Updates
    Added exception handling for VMs that are currently locked for a backup job or migration.
    
    Changed up how emails are sent. They are now passed to a new script called emailalert.sh. This new script will limit the number of duplicate email alerts to 3.

--- loadbalance/loadbalancenodes.sh
+++ loadbalance/loadbalancenodes.sh
@@ -1,20 +1,35 @@
+#!/bin/bash
+
+#Echos out timestamp for logging
+date
+
+#Get vars
 . /proxtools/vars.config
 
+#Set DIR for this script
+SDIR="${DIR}loadbalance"
+
+#Get other values
 lastmove=$(bash /proxtools/loadbalance/checkformovedvm.sh)
 lasthighvm=$(cat /proxtools/loadbalance/lastmigratedvm.txt)
 lastlownode=$(cat /proxtools/loadbalance/lastlownode.txt)
+lasthighnode=$(cat /proxtools/loadbalance/lasthighnode.txt)
+
+#Debug
+echo "Last moved VM: ${lasthighvm}"
+echo "Last high node: ${lasthighnode}"
+echo "Last low node: ${lastlownode}"
+echo "Checking for moved VM: ${lastmove}"
 
 
-
-if [ $lastmove = "NULL" ]; then
+#Check if the last move has completed that migration.
+if [[ "$lastmove" == "Currently Migrating" ]]; then
         echo "Last node has not finished migrating, or an error has occurred."
         echo
         echo "Last Low Node: $lastlownode"
         echo "Last High VM: $lasthighvm"
         echo
-        body="Last node has not finished migrating, or an error has occurred.\nLast Low Node: $lastlownode.\nLast High VM: $lasthighvm.\n"
-        echo -e  "${body}" > "/proxtools/loadbalance/log.txt"
-        echo -e  "From: ${EmailFrom}\nTo: ${EmailRecipients}\nSubject: ProxManager Alert\n\n${body}" | ssmtp ${EmailRecipients}
+        $SDIR/emailalert.sh "CurrentMigration" "$lastmove" "$lastlownode" "ProxManager Alert"
 
         if [ "$lasthighvm" = "NULL" ] || [ "$lastlownode" = "NULL" ] || [ "$lastlownode" = "NULL NULL" ]; then
                 echo "Either lasthighvm or lastlownode is NULL, continuing."
@@ -22,23 +37,40 @@ if [ $lastmove = "NULL" ]; then
         else
                 exit
         fi
+elif [[ "$lastmove" == "$lasthighvm" ]]; then
+        #Successfully moved
+        echo "Successfully moved"
+elif [[ "$lastmove" == "Currently locked: Backup" ]]; then
+        #VM is locked for backing up
+        echo "${lasthighvm} is currently being backed up"
+        $SDIR/emailalert.sh "Backup" "$lasthighvm" "$lastlownode" "ProxManager Alert"
+else
+        #Failed to migrate
+        echo "Failed to migrate"
+        $SDIR/emailalert.sh "FailedToMigrate" "$lastmove" "$lastlownode" "ProxManager Alert"
 fi
+
+
+#Get current high usage node
 highnode=$(bash /proxtools/loadbalance/getmostusednode.sh)
 if [ "$highnode" = "NULL" ]; then
         echo "No highnode found, exiting script"
         echo
         exit
 fi
+#Get current low usage node
 lownode=$(bash /proxtools/loadbalance/getleastusednode.sh)
 if [ "$lownode" = "NULL" ]; then
         echo "No lownode found, exiting script"
         echo
         exit
 fi
+#Debug
 echo "High Node: ${highnode}"
 echo "Low Node: ${lownode}"
 echo
 echo $lownode > "/proxtools/loadbalance/lastlownode.txt"
+echo $highnode > "/proxtools/loadbalance/lasthighnode.txt"
 highvm=$(bash /proxtools/loadbalance/getmostusedvm.sh $highnode)
 echo "High VM: ${highvm}"
 echo
