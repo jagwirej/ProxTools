91726a6f043d28bac193b09bccd972a1784aff6f
jagwirej <131268050+jagwirej@users.noreply.github.com>
2024-05-21 07:28:53 -0500

    Updates
    Added updates folder along with checknodesforupdates.sh to run a check against all nodes to see if there are any pending updates.

---
+++ updates/checknodesforupdates.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Get vars
+. /proxtools/vars.config
+emailbody="The following nodes have pending updates:\n\n"
+
+for node in "${AllServers[@]}"; do
+	#echo "Node:" $node
+
+	Updates=$(ssh root@$node 'apt-get update -qq && apt list --upgradable 2>/dev/null | grep -v "Listing..."')
+	UpdateCount=$(echo -e "$Updates" | wc -l)
+	ActualCount=$(($UpdateCount - 1))
+	if [ $ActualCount -gt 0 ]; then
+		updates=$"TRUE"
+		emailbody="$emailbody\n$node"
+	fi
+done
+
+if [ $updates = "TRUE" ];then
+	echo -e  "From: ${EmailFrom}\nTo: ${EmailRecipients}\nSubject: ProxManager Alerts\n\n${emailbody}" | ssmtp ${EmailRecipients}
+fi
